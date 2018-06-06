bc02e3efab724adb96d3cfab38c874b9e4fae24a
Yang Liu <foamliu@yeah.net>
2018-06-06 18:01:29 +0800

    update

--- count.py
+++ count.py
@@ -2,5 +2,5 @@ import os
 
 
 if __name__ == '__main__':
-    files = [f for f in os.listdir('data') if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+    files = [f for f in os.listdir('image/original') if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
     print('{} files downloaded'.format(len(files)))
