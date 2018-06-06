9a929389a6770fe5b09f16f6c2ca816069451c79
Yang Liu <foamliu@yeah.net>
2018-06-06 20:19:09 +0800

    update

--- count.py
+++ count.py
@@ -4,3 +4,6 @@ import os
 if __name__ == '__main__':
     files = [f for f in os.listdir('image/original') if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
     print('{} files downloaded'.format(len(files)))
+
+    files = [f for f in os.listdir('image/resized') if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+    print('{} files resized'.format(len(files)))
